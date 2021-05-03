9fe7857cd3cfdd3fd28de978fc0809cd24b57873
kthss01 <kthss01@naver.com>
2021-05-03 15:22:36 +0900

    자바스크립트 기초 공부 3일차

---
+++ codeit/index.js
@@ -0,0 +1,12 @@
+// console.log('hello javascript')
+
+// 변수 선언
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 4800;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice * 4 + mochaPrice * 2);
+console.log(lattePrice + mochaPrice);
+console.log(espressoPrice + lattePrice * 4);
+console.log(lattePrice * 2);
