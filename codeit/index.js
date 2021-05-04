4c8d3a22a39e27f29fc02d09ce66ec6239ab02f2
kthss01 <kthss01@naver.com>
2021-05-04 15:24:39 +0900

    자바스크립트 기초 공부 4일차

--- codeit/index.js
+++ codeit/index.js
@@ -23,5 +23,21 @@ greetings('Hola');
 
 // 파라미터
 function welcome(name) {
-  console.log('안녕하세요' + name + '님!');
+  console.log('안녕하세요 ' + name + '님!');
 }
+
+welcome('코드잇');
+
+// 여러개의 파라미터
+function printSum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+printSum(10, 5);
+
+// return문
+function getTwo() {
+  return 2;
+}
+
+console.log(getTwo());
